c517a307993c1b22f1f08c28084d9f3004e988da
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-03-21 23:05:01 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -13,8 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-
-		List<Article> articles = new ArrayList<>();
+		
 		int lastArticleId = 0;
 
 		while (true) {
@@ -34,7 +33,6 @@ public class Main {
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
-				articles.add(article);
 
 				System.out.println(id + "번 글이 생성되었습니다");
 
@@ -85,6 +83,70 @@ public class Main {
 				lastArticleId++;
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				
+				List<Article> articles = new ArrayList<>();
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+					
+					rs = pstmt.executeQuery(sql);
+					
+					while(rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+						
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+				}
+				System.out.println("실행결과 : " + articles);
 
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
