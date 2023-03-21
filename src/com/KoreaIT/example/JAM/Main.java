7c72cded0e8241db1c14fd96fa898548a6b0b109
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2023-03-21 23:46:31 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-		
+
 		int lastArticleId = 0;
 
 		while (true) {
@@ -81,15 +81,69 @@ public class Main {
 					}
 				}
 				lastArticleId++;
+			} else if (cmd.startsWith("article modify")) {
+				int id = Integer.parseInt(cmd.split(" ")[2]);
+				
+				System.out.println("==게시물 수정==");
+				System.out.printf("새 제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("새 내용 : ");
+				String body = sc.nextLine();
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "UPDATE article";
+					sql += " SET UPDATEDATE = NOW(),";
+					sql += " title = '" + title + "',";
+					sql += " `body` = '" + body + "'";
+					sql += " WHERE id = " + id + ";";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					pstmt.executeUpdate();
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+				System.out.println(id + "번 글이 수정되었습니다");
+
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
-				
+
 				Connection conn = null;
 				PreparedStatement pstmt = null;
 				ResultSet rs = null;
-				
+
 				List<Article> articles = new ArrayList<>();
-				
+
 				try {
 					Class.forName("com.mysql.jdbc.Driver");
 					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
@@ -104,16 +158,16 @@ public class Main {
 					System.out.println(sql);
 
 					pstmt = conn.prepareStatement(sql);
-					
+
 					rs = pstmt.executeQuery(sql);
-					
-					while(rs.next()) {
+
+					while (rs.next()) {
 						int id = rs.getInt("id");
 						String regDate = rs.getString("regDate");
 						String updateDate = rs.getString("updateDate");
 						String title = rs.getString("title");
 						String body = rs.getString("body");
-						
+
 						Article article = new Article(id, regDate, updateDate, title, body);
 						articles.add(article);
 					}
@@ -144,7 +198,7 @@ public class Main {
 					} catch (SQLException e) {
 						e.printStackTrace();
 					}
-					
+
 				}
 				System.out.println("실행결과 : " + articles);
 
